74c6dd6d3830c2d3b9f1c806157a59f8d75f89d7
André <andre.escocard@nttdata.com>
2022-03-22 17:18:52 -0300

    feat: create main component

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    JogoDaVelhaComponent
   ]
 })
 export class JogoDaVelhaModule { }
