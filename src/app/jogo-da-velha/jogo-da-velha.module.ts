3c354b3e035c1b1f76c4353d4264eb4f730667b1
André <andre.escocard@nttdata.com>
2022-03-22 17:22:14 -0300

    feat: create main service

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
-
+import { JogoDaVelhaService } from './shared';
 
 
 @NgModule({
@@ -13,6 +13,9 @@ import { JogoDaVelhaComponent } from './jogo-da-velha.component';
   ],
   exports: [
     JogoDaVelhaComponent
+  ], 
+  providers: [
+    JogoDaVelhaService
   ]
 })
 export class JogoDaVelhaModule { }
