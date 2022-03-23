010c85aa06190aea455baead9a83f7232ef527ba
André <andre.escocard@nttdata.com>
2022-03-23 10:46:11 -0300

    feat: start page of game

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { JogoDaVelhaService } from './shared';
+
 @Component({
   selector: 'app-jogo-da-velha',
   templateUrl: './jogo-da-velha.component.html',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoDaVelhaComponent implements OnInit {
 
-  constructor() { }
+  constructor( private jogoDaVelhaService: JogoDaVelhaService) { }
 
   ngOnInit(): void {
+    this.jogoDaVelhaService.inicializar();
   }
+
+   /**
+   * Retorna se a tela de início deve ser exibida.
+   * 
+   * @return boolean
+   */
+    get showInicio(): boolean {
+      return this.jogoDaVelhaService.showInicio;
+    }
+  
+    /**
+     * Retorna se o tabuleiro deve ser exibido.
+     * 
+     * @return boolean
+     */
+    get showTabuleiro(): boolean {
+      return this.jogoDaVelhaService.showTabuleiro;
+    }
+  
+    /**
+     * Retorna se a tela de fim de jogo deve ser exibida.
+     * 
+     * @return boolean
+     */
+    get showFinal(): boolean {
+      return this.jogoDaVelhaService.showFinal;
+    }
+
+    /**
+     * Inicializa os dados de um novo jogo.
+     *
+     * @return void
+     */
+    iniciarJogo(): void {
+      this.jogoDaVelhaService.iniciarJogo();
+    }
 
 }
